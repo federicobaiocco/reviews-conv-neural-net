15072df77130932a78a5704e4a1739339a0192a7
Federico Baiocco <baioccofede@gmail.com>
2020-05-31 17:57:33 -0300

    db collections and api endpoints for reviews and train_data

--- reviews_cnn_model/app.py
+++ reviews_cnn_model/app.py
@@ -2,16 +2,23 @@ from flask import Flask,request,jsonify
 from reviews_cnn_package.model.predict import predict
 from reviews_cnn_package.model.train_pipeline import train_model
 from reviews_cnn_package.config import config
-
-'''
-Deploy app to heroku: 
-git subtree push --prefix flask-api heroku master 
-'''
+import os
+import json
+import datetime
+from bson.objectid import ObjectId
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
+if(os.environ.get('env') == 'prod'):
+    app.config['MONGO_URI'] = os.environ.get('DB')
+else:
+    app.config['MONGO_URI'] = 'mongodb://localhost:27017/reviews_db'
+mongo = PyMongo(app)
+
 @app.route('/')
 def index():
-    return "Index Page"
+    return "Reviews"
 
 @app.route('/health', methods=['GET'])
 def health():
@@ -30,6 +37,95 @@ def make_prediction():
         return jsonify(
             {'prediction': str(result['prediction']),
              'model_version': str(config.MODEL_VERSION)})
+
+@app.route('/reviews', methods=['GET', 'POST', 'PATCH'])
+def review():
+    '''
+    review: {
+        text: String,
+        prediction: Bool,  # 1 = Approved ; 0 = Disapproved
+        true_label: Bool,  # 1 = Approved ; 0 = Disapproved
+        checked: Bool
+    }
+    '''
+    request_json = request.json
+
+    if request.method == 'GET':
+        documents = mongo.db.reviews.find({})
+        response = []
+        for document in documents:
+            document['_id'] = str(document['_id'])
+            response.append(document)
+        return jsonify(response[:10]), 200
+
+    if request.method == 'POST':
+        if request_json.get('text', None) is not None and \
+            request_json.get('prediction', None) is not None:
+            request_json['checked'] = False # Default value
+            request_json['true_label'] = None # Default value
+            mongo.db.reviews.insert_one(request_json)
+            return jsonify({'OK': True, 'message': 'Created'}), 200
+        else:
+            return jsonify({'OK': False, 'message': 'Required params are missing'}), 400
+
+    if request.method == 'PATCH':
+        query = request_json.get('query', {})
+        query['_id'] = ObjectId(query['_id'])
+        payload = request_json.get('payload', {})
+        if query != {}:
+            mongo.db.reviews.update_one(
+                query, {'$set': payload})
+            
+            if(payload['checked'] == 1):
+                review = mongo.db.reviews.find_one(query, {"_id": False, "text": True, "true_label": True})
+                train_review = {
+                    "text": review['text'],
+                    "state": review['true_label']
+                }
+                mongo.db.train_data.insert_one(train_review)
+
+            return jsonify({'OK': True, 'message': 'Record updated'}), 200
+        else:
+            return jsonify({'OK': False, 'message': 'Required params are missing'}), 400
+
+@app.route('/reviews/approved', methods=['GET'])
+def approved_reviews():
+    if request.method == 'GET':
+        documents = mongo.db.reviews.find({
+            "$or" : [
+                {"$and": [
+                    {"checked": 1},
+                    {"true_label": 1}
+                ]},
+                {"$and": [
+                    {"checked": 0},
+                    {"prediction": 1}
+                ]}
+            ]
+            })
+        response = []
+        for document in documents:
+            document['_id'] = str(document['_id'])
+            response.append(document)
+        return jsonify(response[:10]), 200
+
+@app.route('/train_data', methods=['GET', 'POST', 'PATCH'])
+def train_data():
+    '''
+    train_data: {
+        text: String,
+        state: Bool -> 1 = Approved ; 0 = Disapproved
+    }
+    '''
+    request_json = request.json
+
+    if request.method == 'GET':
+        documents = mongo.db.train_data.find({})
+        response = []
+        for document in documents:
+            document['_id'] = str(document['_id'])
+            response.append(document)
+        return jsonify(response), 200
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True)
